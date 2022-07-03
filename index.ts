1c151fd433e34f3e659ac801703390dde6744b97
Shivam <singhshivamkr@gmail.com>
2022-07-03 23:20:25 +0530

    :tada: Create image import/export functionality

--- index.ts
+++ index.ts
@@ -12,63 +12,36 @@ const cwd = process.cwd();
 
 interface Opts {
   dir: string;
-  trim: number;
   output: string;
   extension: string[];
   name: string;
-  keep: boolean;
-  ignore: string[];
   watch: boolean;
 }
 
-program.name("path-types");
+program.name("Image import generator");
 program.description("Generate file path types");
 
 program
   .addOption(
     new Option(
       "-d, --dir [directory]",
-      "Relative path of the directory, whose paths you want to generate"
+      "Relative path of the image directory"
     ).default("./", "current dir")
   )
   .addOption(
     new Option("-o, --output [output]", "Output file location").default(
-      "./FilePaths.ts",
-      "FilePaths.ts"
+      "./Images.ts",
+      "Images.ts"
     )
   )
   .addOption(
     new Option(
       "-e, --extension [extension...]",
-      "File extension, eg. ts, js"
-    ).default("ts", "ts")
-  )
-  .addOption(
-    new Option("-n, --name [name]", "Type name").default(
-      "FilePaths",
-      "FilePaths"
-    )
-  )
-  .addOption(
-    new Option(
-      "-t, --trim <number>",
-      "Trim characters from end of path"
-    ).default(0, "None")
-  )
-  .addOption(
-    new Option("-i, --ignore [ignore...]", "ignore paths").default(
-      "node_modules",
-      "node_modules"
-    )
+      "File extension, eg. jpg, png, svg"
+    ).default(["jpg", "jpeg", "gif", "png", "svg", "webp"], "jpg, svg, png ...")
   )
   .addOption(
     new Option("-w, --watch", "Enable watching").default(false, "disabled")
-  )
-  .addOption(
-    new Option("-k, --keep", "Keep file extension name").default(
-      false,
-      "disabled"
-    )
   );
 
 program.parse(process.argv);
@@ -82,69 +55,59 @@ const glob = `**/*.{${
     ? options.extension.join(",")
     : options.extension
 },}`;
-// output -> "**/*.{js,ts}"[Symbol]
+// output -> "**/*.{png,webp,svg}"
 
 const absolutePath = join(cwd, options.dir);
 
-const removeFileExt = (path: string) => {
-  // split them, so that we can remove the extension
-  const pathArr = path.split(".");
-  // file ext will be in last idx
-  pathArr.pop();
-  //stitch them back
-  return pathArr.join(".");
-};
-
-const generateTypes = () => {
+const generateImports = () => {
   let list = fg.sync(glob, {
     cwd: absolutePath,
-    ignore: options.ignore,
   });
 
   // console.log(glob, list);
 
-  const { keep, trim } = options;
-  // remove extension if keep is false or trim is needed
-  if (!keep || trim) {
-    list = list.map((path) => {
-      // only remove extension if user specifies
-      const pathStr = keep ? path : removeFileExt(path);
-      // remove chars from str if specied
-      return trim ? pathStr.slice(0, -trim) : pathStr;
-    });
-  }
-
   // put the paths array in set to remove any duplicates
   const listSet = new Set([...list]);
 
-  let unionList = [...listSet].join('"\n  | "');
+  let importsStr = "";
+  let exportsStr = "";
 
-  const str = `export type ${options.name} =
-  | "${unionList}";`;
+  listSet.forEach((path) => {
+    let filename = path.split("/").at(-1);
+    filename = filename?.split(".").at(0);
 
-  writeFileSync(join(cwd, options.output), str);
+    if (!filename) {
+      throw Error("Failed to extract image name");
+    }
+
+    importsStr += `import _${filename} from "./${path}";\n`;
+    exportsStr += `export const ${filename} = _${filename};\n`;
+  });
+
+  const finalStr = importsStr + "\n" + exportsStr;
+
+  writeFileSync(join(cwd, options.output), finalStr);
 
   console.log(green(bold(`Types generated in ${options.output}.\n`)));
 };
 
 // run generate function once
-generateTypes();
+generateImports();
 
 if (options.watch) {
   const chokidar = await import("chokidar");
   chokidar
     .watch(glob, {
       cwd: absolutePath,
-      ignored: [...options.ignore, options.output],
       ignoreInitial: true,
     })
     .on("add", (filename) => {
-      console.log(green(`New file added ${filename}`));
-      generateTypes();
+      console.log(green(`New image added ${filename}`));
+      generateImports();
     })
     .on("unlink", (filename) => {
-      console.log(red(`File removed ${filename}`));
-      generateTypes();
+      console.log(red(`Image removed ${filename}`));
+      generateImports();
     })
     .on("ready", () => console.log(yellow("Watch mode enabled!")));
 }
